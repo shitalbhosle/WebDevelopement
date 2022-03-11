8eb29adb9058625a0bff556e16b1889a9065f1df
shitalbhosle <94833435+shitalbhosle@users.noreply.github.com>
2022-03-11 10:55:06 +0530

    nodeWithExpress

---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+var mySuperHeroes = superheroes.random();
+
+console.log(mySuperHeroes);
+
+var supervillains = require("supervillains");
+var mySuperVillians = supervillains.random();
+console.log(mySuperVillians);
